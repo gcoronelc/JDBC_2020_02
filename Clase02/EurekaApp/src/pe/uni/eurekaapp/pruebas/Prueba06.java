f884d06660fa26945b2f6b650f60eb25f2d308b9
Gustavo Coronel <gcoronelc@gmail.com>
2020-09-20 10:16:37 -0500

    Clase 02

--- Clase02/EurekaApp/src/pe/uni/eurekaapp/pruebas/Prueba06.java
+++ Clase02/EurekaApp/src/pe/uni/eurekaapp/pruebas/Prueba06.java
@@ -1,8 +1,10 @@
 package pe.uni.eurekaapp.pruebas;
 
+import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLType;
 import pe.uni.eurekaapp.db.AccesoDB;
 
 /**
@@ -12,23 +14,21 @@ import pe.uni.eurekaapp.db.AccesoDB;
  * @youtube www.youtube.com/c/DesarrollaSoftware
  * @facebook www.facebook.com/groups/desarrollasoftware/
  */
-public class Prueba05 {
+public class Prueba06 {
 	
 	public static void main(String[] args) {
 		Connection cn = null;
 		try {
 			cn = AccesoDB.getConnection();
-			String sql = "select * from eureka.moneda where upper(vch_monedescripcion) like ?";
-			PreparedStatement pstm = cn.prepareStatement(sql);
-			pstm.setString(1, "%SOLES%");
-			ResultSet rs = pstm.executeQuery();
-			while( rs.next() ){
-				String codigo = rs.getString("chr_monecodigo");
-				String descripcion = rs.getString("vch_monedescripcion");
-				System.out.println(codigo + " - " + descripcion);
-			}
-			rs.close();
-			pstm.close();
+			String sql = "{call usp_sumar(?,?,?)}";
+			CallableStatement cstm = cn.prepareCall(sql);
+			cstm.setDouble(1, 45.78);
+			cstm.setDouble(2, 56.23);
+			cstm.registerOutParameter(3, java.sql.Types.DECIMAL);
+			cstm.executeUpdate();
+			double suma = cstm.getDouble(3);
+			cstm.close();
+			System.out.println("Suna: " + suma);
 			System.out.println("Proceso ok.");
 		} catch (Exception e) {
 			e.printStackTrace();
